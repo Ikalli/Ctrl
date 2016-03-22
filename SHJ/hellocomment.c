a8e26b51399d026dc1cd12d0d947a548d3d922ce
simhejun <tlagmlwnsnet@naver.com>
2016-03-22 19:59:57 +0900

    Create hellocomment.c

---
+++ SHJ/hellocomment.c
@@ -0,0 +1,14 @@
+/*
+제목: Hello world 출력하기
+기능: 문자열의 출력
+파일이름: Hellocomment.c
+*/
+#include <stdio.h> //헤더파일 선언
+
+int main(void)  // main 함수의시작
+{
+	/*
+	 */
+	printf("hello world \n");  //
+	return 0;  //0의 반환
+}// main 함수의끝
