c8655b5ae76c32c6eaa78cc33439db55c5d703fb
joseungdo <gkdldnjsfn@naver.com>
2016-03-23 14:34:12 +0900

    Create Mentorig_p39_q2.c

---
+++ JSD/Homework/Mentorig_p39_q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (void)
+{
+	printf ("-이름-\n조승도\n");
+	printf ("\n-주소-\n기본주소: 대한민국 경기도 용인시 기흥구 신갈동\n세부주소: 40-24 302호\n");
+	printf ("\n-전화번호-\n+82 010-9149-2753\n");
+	return 0
+}
