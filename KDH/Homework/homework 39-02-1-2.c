841845c41bca1dd2091106df9a5bd2cd0b4869e3
jamescloud414 <ehdgus00414@naver.com>
2016-03-28 23:39:12 +0900

    Create homework 39-02-1-2.c

---
+++ KDH/Homework/homework 39-02-1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("이    름: 김동현\n");
+    printf("주    소: 경기도 용인시 기흥구 보정동 용구대로 2518번길 15\n");
+    printf("전화번호: 010-4098-0883\n");
+    return 0;
+}
